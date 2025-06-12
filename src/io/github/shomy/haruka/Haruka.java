d00979413f4a7024d99dee486955be7e9d343078
Shomy 🍂 <61943525+shomykohai@users.noreply.github.com>
2025-06-12 02:14:55 +0200

    Bump version

--- src/io/github/shomy/haruka/Haruka.java
+++ src/io/github/shomy/haruka/Haruka.java
@@ -7,6 +7,6 @@ public class Haruka {
 	@DexAdd
 	public static final String TAG = "Haruka";
 	@DexAdd
-	public static final String VERSION = "0.2";
+	public static final String VERSION = "0.3";
 
 }
